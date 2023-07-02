3d0b0ed8fa67f65676c275b95f75db4a983f5a98
Jacob Debel <jacobdebel@gmail.com>
2023-07-02 19:28:12 +0200

    Flere grafer i samme koordinatsystem

---
+++ 02_Plot_af_data/tre_grafer_i_samme_koordinatsystem.py
@@ -0,0 +1,39 @@
+# [[file:02_Plot_af_data.org::*Flere grafer i samme koordinatsystem][Flere grafer i samme koordinatsystem:1]]
+import csv
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+tider = []  # Liste, som skal indeholde alle tider i float
+# Lister, som skal indeholde temperatuerne for hver af de 3 kaffer
+temperaturer_1 = []
+temperaturer_2 = []
+temperaturer_3 = []
+
+with open("tre_afkoelingskurver.csv") as datafil:
+    csv_laeser = csv.reader(datafil, delimiter=",")
+    next(csv_laeser)  # Springer første linje over
+    for linje in csv_laeser:
+        # På højre side af lighedstegnet anvendes en list comprehension
+        # hvor hvert element omdannes til float
+        # På venstre side udpakkes den nye liste til variablen tid og listen temperaturer
+        tid, temp_1, temp_2, temp_3 = [float(element) for element in linje]
+
+        # Tid og temperatur tilføjes til listerne tider og temperaturer
+        tider.append(tid)
+        temperaturer_1.append(temp_1)
+        temperaturer_2.append(temp_2)
+        temperaturer_3.append(temp_3)
+
+# Denne del sørger for plot af data
+plt.style.use("seaborn-v0_8")
+fig, ax = plt.subplots(layout="constrained")
+ax.plot(tider, temperaturer_1, ".", label="Kaffe 1")
+ax.plot(tider, temperaturer_2, ".", label="Kaffe 2")
+ax.plot(tider, temperaturer_3, ".", label="Kaffe 3")
+ax.set_title("Kaffens temperatur som funktion af tiden.")
+ax.set_xlabel("Tid [min]")
+ax.set_ylabel(r"Temperatur [${}^\circ C$]")
+ax.legend()
+plt.show()
+# Flere grafer i samme koordinatsystem:1 ends here
---
+++ 02_Plot_af_data/tre_grafer_i_samme_koordinatsystem_opgave.py
@@ -0,0 +1,33 @@
+# [[file:02_Plot_af_data.org::*Flere grafer i samme koordinatsystem][Flere grafer i samme koordinatsystem:2]]
+import csv
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+tider = []  # Liste som skal indeholde alle tider i float
+# Dict som skal indeholde en liste af alle temperaturer i float for hver kaffe
+temperaturer = {1: [], 2: [], 3: [],}  with open("tre_afkoelingskurver.csv") as datafil:
+    csv_laeser = csv.reader(datafil, delimiter=",")
+    next(csv_laeser)  # Springer første linje over
+    for linje in csv_laeser:
+        # På højre side af lighedstegnet anvendes en list comprehension
+        # hvor hvert element omdannes til float
+        # På venstre side udpakkes den nye liste til variablen tid og listen temperaturer
+        tid, *_temperaturer = [float(element) for element in linje]
+
+        # Tid og temperatur tilføjes til listerne tider og temperaturer
+        tider.append(tid)
+        for run, temp in zip(temperaturer, _temperaturer):
+            temperaturer[run].append(temp)
+
+# Denne del sørger for plot af data
+plt.style.use("seaborn-v0_8")
+fig, ax = plt.subplots(layout="constrained")
+for nummer, _temperaturer in temperaturer.items():
+    ax.plot(tider, _temperaturer, ".", label=f"Kaffe {nummer}")
+ax.set_title("Kaffens temperatur som funktion af tiden.")
+ax.set_xlabel("Tid [min]")
+ax.set_ylabel(r"Temperatur [${}^\circ C$]")
+ax.legend()
+plt.show()
+# Flere grafer i samme koordinatsystem:2 ends here
